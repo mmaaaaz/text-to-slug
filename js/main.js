1e4b51ffabb1d1a1f9ae27716610f471de909f60
Maaz <devmaaaaz@gmail.com>
2023-07-14 12:42:13 +0500

    Updated as per original repo

--- js/main.js
+++ js/main.js
@@ -1,28 +1,45 @@
-const input = document.querySelector("#rawText");
-const output = document.querySelector("#sluggedText");
-const copyToClipboard = document.querySelector("#copyToClipboard");
+const getElement = (id) => document.querySelector(id);
+const input = getElement("#rawText");
+const output = getElement("#sluggedText");
+const separator = getElement("#separator");
+const clearInput = getElement("#clearInput");
+const copyToClipboard = getElement("#copyToClipboard");
 
-input.addEventListener("input", () => (output.value = slugify(input.value)));
-copyToClipboard.addEventListener("click", copyOutputToClipboard);
+const updateOutput = () => (output.value = slugify(input.value));
 
-const copyOutputToClipboard = () => {
-  if (output.value.length === 0) return;
-  if (input.value === output.value) return;
-
-  copyToClipboard.innerHTML = "Copied to Clipboard!";
-
-  navigator.clipboard.writeText(output.value).then(() => {
-    setTimeout(() => {
-      copyToClipboard.innerHTML = "Copy to Clipboard";
-    }, 1000);
-  });
+const clearInputValues = () => {
+  input.value = output.value = "";
+  input.focus();
 };
-
-const slugify = (text) =>
-  replaceAccentedCharacters(text)
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/(^-|-$)+/g, "");
 
 const replaceAccentedCharacters = (text) =>
   text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+const slugify = (text) =>
+  replaceAccentedCharacters(text)
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, separator.value)
+    .replace(/(^-|-$)+/g, "");
+
+const copyTextToClipboard = () => {
+  if (output.value.length === 0) return;
+
+  copyToClipboard.innerHTML = "Copied to Clipboard!";
+  copyToClipboard.disabled = true;
+
+  navigator.clipboard.writeText(output.value);
+
+  setTimeout(() => {
+    copyToClipboard.innerHTML = "Copy to Clipboard";
+    copyToClipboard.disabled = false;
+  }, 400);
+};
+
+input.addEventListener("input", updateOutput);
+separator.addEventListener("input", () => {
+  updateOutput();
+  input.focus();
+});
+clearInput.addEventListener("click", clearInputValues);
+copyToClipboard.addEventListener("click", copyTextToClipboard);
